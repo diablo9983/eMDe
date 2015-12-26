4dd553247079786d3775b615f7884dc41341a603
diablo9983 <diablo9983@gmail.com>
2015-12-26 17:03:20 +0100

    Initial code

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function (grunt) {
+
+  'use strict';
+
+  grunt.initConfig({
+    watch: {
+      css: {
+        files: ["src/scss/*.scss", "src/scss/elements/*.scss"],
+        tasks: ["sass"]
+      },
+      jade: {
+        files: ["src/jade/**/*.jade"],
+        tasks: ["jade"]
+      },
+      js: {
+        files: ["src/js/**/*.js"],
+        tasks: ["uglify"]
+      }
+    },
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'dist/css/emde.css': 'src/scss/emde.scss'
+        }
+      }
+    },
+    jade: {
+      compile: {
+        options: {
+          pretty: true
+        },
+        files: [{
+          'dist/index.html': 'src/jade/index.jade'
+        }, {
+          cwd: 'src/jade/pages',
+          src: '*.jade',
+          dest: 'dist/pages',
+          expand: true,
+          ext: '.html'
+        }]
+      }
+    },
+    uglify: {
+      js: {
+        options: {
+          sourceMap: true
+        },
+        files: {
+          'dist/js/emde.js': 'src/js/emde.js'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // The default task (running "grunt" in console) is "watch"
+  grunt.registerTask('default', ['watch']);
+}

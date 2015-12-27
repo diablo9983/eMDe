1e46d28278c61bc523d981dae075d26795298bb9
diablo9983 <diablo9983@gmail.com>
2015-12-27 22:12:29 +0100

    Sidebar account menu

--- src/js/emde.js
+++ src/js/emde.js
@@ -4,6 +4,7 @@
   var Sidebar = function() {
     this.menuItem = $('#sidebar-menu li > a');
     this.sidebarMenu = $('#sidebar-menu');
+    this.sidebarAccount = $('.sidebar-account');
   }
   Sidebar.prototype.toggleItem = function(element,action) {
     if(!element.length || !this.sidebarMenu.find(element).length || $.inArray(action,['open','close']) === -1) return;
@@ -19,12 +20,25 @@
   Sidebar.prototype.openItem = function(element) {
     this.toggleItem(element,'open');
   }
+  Sidebar.prototype.toggleAccountMenu = function() {
+    var menu = this.sidebarAccount.children('.sidebar-account-menu');
+    if(this.sidebarAccount.hasClass('open')) {
+      this.sidebarAccount.removeClass('open');
+      menu.slideUp();
+    } else {
+      this.sidebarAccount.addClass('open');
+      menu.slideDown();
+    }
+  }
   Sidebar.prototype.init = function () {
     var $this = this;
     $this.menuItem.on('click', function() {
       var $parent = $(this).parent('li');
       $this.closeItem($parent.siblings('li.active'));
       $this.toggleItem($parent,($parent.hasClass('active') ? 'close' : 'open'));
+    });
+    $this.sidebarAccount.children('a').on('click', function(){
+      $this.toggleAccountMenu();
     });
   }
 

11175c1596ebc682d3785fde5d7d7bfd0d987d90
diablo9983 <diablo9983@gmail.com>
2015-12-27 16:53:40 +0100

    Sidebar

--- src/js/emde.js
+++ src/js/emde.js
@@ -3,20 +3,28 @@
 
   var Sidebar = function() {
     this.menuItem = $('#sidebar-menu li > a');
+    this.sidebarMenu = $('#sidebar-menu');
   }
-  Sidebar.prototype.openItem = function(el) {
-
+  Sidebar.prototype.toggleItem = function(element,action) {
+    if(!element.length || !this.sidebarMenu.find(element).length || $.inArray(action,['open','close']) === -1) return;
+    if(element.is('a')) element = element.parent('li');
+    var $ul = element.children('ul');
+    if($ul.length == 0) return;
+    element[(action === 'open' ? 'add' : 'remove') + 'Class'].call(element,'active');
+    $ul['slide' + (action === 'open' ? 'Down' : 'Up')].call($ul);
   }
-  Sidebar.prototype.closeItem = function(el) {
-
+  Sidebar.prototype.closeItem = function(element) {
+    this.toggleItem(element,'close');
+  }
+  Sidebar.prototype.openItem = function(element) {
+    this.toggleItem(element,'open');
   }
   Sidebar.prototype.init = function () {
     var $this = this;
-
     $this.menuItem.on('click', function() {
       var $parent = $(this).parent('li');
       $this.closeItem($parent.siblings('li.active'));
-      $this[($parent.hasClass('active') ? 'close' : 'open') + 'Item'].call($this,$(this));
+      $this.toggleItem($parent,($parent.hasClass('active') ? 'close' : 'open'));
     });
   }
 
